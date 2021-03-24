5cfe21c4a4338e833b1bd11a3740e5b25bcd03b7
TarasOrlovskyi <taras.orlovskyi@gmail.com>
2021-03-24 17:13:24 +0200

    Added get vinyl functionality
    1. Added VinylShopService;
    2. Added ShopService;
    3. Added Tests for VinylShopService.

---
+++ src/main/java/com/vinylteam/vinyl/service/impl/VinylShopService.java
@@ -0,0 +1,104 @@
+package com.vinylteam.vinyl.service.impl;
+
+import com.vinylteam.vinyl.entity.Vinyl;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import com.vinylteam.vinyl.service.ShopService;
+
+import java.io.IOException;
+import java.util.*;
+
+public class VinylShopService implements ShopService {
+    private final String startLink = "http://vinyl.ua";
+
+    HashSet<String> getShowcaseLinks() throws IOException {
+        HashSet<String> showcaseLinks = new HashSet<>();
+        Document doc = Jsoup.connect(startLink).get();
+        Elements innerLinks;
+        innerLinks = doc.getElementsByTag("a");
+
+        for (Element innerLink : innerLinks) {
+            String link;
+            if (innerLink.attr("href").contains("www") || innerLink.attr("href").contains("http")) {
+                link = innerLink.attr("href");
+            } else {
+                link = startLink + innerLink.attr("href");
+            }
+            if (link.contains("http://vinyl.ua/showcase/")) {
+                showcaseLinks.add(link);
+            }
+        }
+
+        return showcaseLinks;
+    }
+
+    HashSet<String> getPageLinks(HashSet<String> showcaseLinks) throws IOException {
+        LinkedHashSet<String> pageLinks = new LinkedHashSet<>();
+
+        for (String showcaseLink : showcaseLinks) {
+            Document doc = Jsoup.connect(showcaseLink).get();
+            Elements innerLinks = doc.getElementsByTag("a");
+
+            for (Element innerLink : innerLinks) {
+                String link;
+                if (innerLink.attr("href").contains("www") || innerLink.attr("href").contains("http")) {
+                    link = innerLink.attr("href");
+                } else {
+                    link = startLink + innerLink.attr("href");
+                }
+                if (link.contains("?page=") && !link.contains("ussr?page=2")) {
+                    pageLinks.add(link);
+                }
+            }
+        }
+
+        return pageLinks;
+    }
+
+
+    HashSet<Vinyl> readProductDataFromPage(HashSet<String> pageLinks) throws IOException {
+        HashSet<Vinyl> dataOfProducts = new HashSet<>();
+
+        for (String pageLink : pageLinks) {
+            Document doc = Jsoup.connect(pageLink).get();
+            Elements pageElements = doc.getElementsByClass("vinyl-release showcase");
+            for (Element pageElement : pageElements) {
+                String release = pageElement.getElementsByClass("margin-top-clear margin-bot-5").text();
+                String artist = pageElement.getElementsByClass("text-ellipsis").select("a").text();
+
+                //FIXME http://vinyl.ua/release/567/various-phatmix-volume-2 - there is problem, because there is no teg <a>, because there is no name Artist
+                if (artist.equals("")) {
+                    artist = pageElement.getElementsByClass("text-ellipsis").select("span").text();
+                }
+
+                String price = pageElement.getElementsByClass("pull-left margin-top-5 showcase-release-price").text();
+                String vinylLink = startLink + pageElement.getElementsByClass("img-showcase-release").select("a").attr("href");
+                String[] imageLinks = pageElement.getElementsByClass("img-showcase-release").attr("style").split("'");
+                String imageLink = imageLinks[1];
+                String[] genres = pageLink.split("[/?]");
+                String genre = genres[4];
+
+                Vinyl vinyl = new Vinyl();
+                vinyl.setRelease(release);
+                vinyl.setArtist(artist);
+                vinyl.setPrice(price);
+                vinyl.setVinylLink(vinylLink);
+                vinyl.setImageLink(imageLink);
+                vinyl.setGenre(genre);
+                dataOfProducts.add(vinyl);
+            }
+        }
+
+        return dataOfProducts;
+    }
+
+    @Override
+    public List<Vinyl> getDataProduct() throws IOException {
+        HashSet<String> showcaseLinks = getShowcaseLinks();
+        HashSet<String> pageLinks = getPageLinks(showcaseLinks);
+        HashSet<Vinyl>  vinyls = readProductDataFromPage(pageLinks);
+        return new ArrayList<>(vinyls);
+    }
+}
---
+++ src/main/test/com/vinylteam/vinyl/service/impl/VinylShopServiceTest.java
@@ -0,0 +1,44 @@
+package com.vinylteam.vinyl.service.impl;
+
+import com.vinylteam.vinyl.entity.Vinyl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VinylShopServiceTest {
+    private VinylShopService vinylShopService;
+    private HashSet<String> oneTestLink;
+
+    @BeforeEach
+    void setUp() {
+        vinylShopService = new VinylShopService();
+        oneTestLink = new HashSet<>();
+        oneTestLink.add("http://vinyl.ua/showcase/reggae");
+    }
+
+    @Test
+    void getShowcaseLinksTest() throws IOException {
+        assertFalse(vinylShopService.getShowcaseLinks().isEmpty());
+    }
+
+    @Test
+    void getPageLinksTest() throws IOException {
+        assertFalse(vinylShopService.getPageLinks(oneTestLink).isEmpty());
+    }
+
+    @Test
+    void readProductDataFromPageTest() throws IOException {
+        assertFalse(vinylShopService.readProductDataFromPage(oneTestLink).isEmpty());
+    }
+
+    @Test
+    void getDataProduct() throws IOException {
+        List<Vinyl> actualVinyls = vinylShopService.getDataProduct();
+        assertFalse(actualVinyls.isEmpty());
+    }
+}
