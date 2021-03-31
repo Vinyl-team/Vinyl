951c7f7eb80c35c2db9a56a8402d95f14631ad07
AlexKievDeveloper <glushkov18@gmail.com>
2021-03-31 11:34:30 +0300

    Changed logback.xml
    1. Added logs and comments to JdbcUserDaoITest.

--- src/test/java/com/vinylteam/vinyl/dao/jdbc/JdbcUserDaoITest.java
+++ src/test/java/com/vinylteam/vinyl/dao/jdbc/JdbcUserDaoITest.java
@@ -4,6 +4,8 @@ import com.vinylteam.vinyl.dao.DBDataSource;
 import com.vinylteam.vinyl.entity.Role;
 import com.vinylteam.vinyl.entity.User;
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JdbcUserDaoITest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
     private final DBDataSource dbDataSource = new DBDataSource();
     private final JdbcUserDao jdbcUserDao = dbDataSource.getJDBCUserDAO();
@@ -27,7 +30,11 @@ class JdbcUserDaoITest {
 
     @BeforeAll
     void beforeAll() throws SQLException {
+        logger.info("DB name: {}", dbDataSource.getDataSource().getDatabaseName());
         connection = dbDataSource.getDataSource().getConnection();
+        try (Statement truncateStatement = connection.createStatement()) {
+            truncateStatement.executeUpdate(TRUNCATE_TABLE_RESTART_IDENTITY);
+        }
     }
 
     @AfterAll
@@ -37,12 +44,12 @@ class JdbcUserDaoITest {
 
     @BeforeEach
     void beforeEach() throws SQLException {
-        try (Statement truncateStatement = connection.createStatement()) {
-
-            truncateStatement.executeUpdate(TRUNCATE_TABLE_RESTART_IDENTITY);
+/*        try (Statement truncateStatement = connection.createStatement()) {
+            logger.info("DB name {}", dbDataSource.getDataSource().getDatabaseName());
+            truncateStatement.executeUpdate(TRUNCATE_TABLE_RESTART_IDENTITY);*/
             try (PreparedStatement insertStatement = connection
                     .prepareStatement(INSERT_INTO_TABLE)) {
-
+                logger.info("I am adding two rows to table users before the method!");
                 insertStatement.setString(1, "testuser1@vinyl.com");
                 insertStatement.setString(2, "HASH1");
                 insertStatement.setString(3, "");
@@ -55,15 +62,25 @@ class JdbcUserDaoITest {
                 insertStatement.setString(10, Role.USER.toString());
                 insertStatement.executeUpdate();
             }
+        //}
+    }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        logger.info("I am cleaning table users after the method!");
+        try (Statement truncateStatement = connection.createStatement()) {
+            truncateStatement.executeUpdate(TRUNCATE_TABLE_RESTART_IDENTITY);
         }
     }
 
     @Test
+    @DisplayName("Checks the number of rows")
     void countAllFilledTest() {
         assertEquals(2, jdbcUserDao.countAll());
     }
 
     @Test
+    @DisplayName("Checks the number of rows if table is empty")
     void countAllEmptyTest() throws SQLException {
         try (Statement truncateStatement = connection.createStatement()) {
             truncateStatement.executeUpdate(TRUNCATE_TABLE_RESTART_IDENTITY);
@@ -72,6 +89,7 @@ class JdbcUserDaoITest {
     }
 
     @Test
+    @DisplayName("Gets an existing user from db")
     void getByExistingEmail() {
         User userGottenByExistingEmail;
         userGottenByExistingEmail = jdbcUserDao.getByEmail("testuser1@vinyl.com");
@@ -85,14 +103,15 @@ class JdbcUserDaoITest {
     }
 
     @Test
+    @DisplayName("Gets not existing user from db")
     void getByNotExistingEmail() {
-
         User userGottenByNonexistentEmail = jdbcUserDao.getByEmail("testuser3@vinyluserGottenByExistingEmail.com");
         assertNull(userGottenByNonexistentEmail);
         assertEquals(2, jdbcUserDao.countAll());
     }
-
+    // FIXME
     @Test
+    @DisplayName("Gets user by null email from db")
     void getByNullEmail() {
         User userGottenByNullEmail = jdbcUserDao.getByEmail(null);
         assertNull(userGottenByNullEmail);
@@ -100,10 +119,13 @@ class JdbcUserDaoITest {
     }
 
     @Test
-    void addNewTest() {
+    @DisplayName("Adds user to db")
+    void addUserTest() {
+        //when
         assertTrue(jdbcUserDao.add(new User("newtestuser3@vinyl.com",
                 "HASH3", Base64.getDecoder().decode(""), 0, Role.USER)));
 
+        //then
         assertEquals(3, jdbcUserDao.countAll());
         User addedUser = jdbcUserDao.getByEmail("newtestuser3@vinyl.com");
 
@@ -115,6 +137,7 @@ class JdbcUserDaoITest {
     }
 
     @Test
+    @DisplayName("Adds existing user with the same password")
     void addExistingWithSamePasswordTest() {
         assertFalse(jdbcUserDao.add(new User("testuser2@vinyl.com",
                 "HASH2", Base64.getDecoder().decode(""), 0, Role.USER)));
@@ -122,13 +145,16 @@ class JdbcUserDaoITest {
     }
 
     @Test
+    @DisplayName("Adds existing user with new password")
     void addExistingWithNewPasswordTest() {
         assertFalse(jdbcUserDao.add(new User("testuser2@vinyl.com",
                 "HASH3", Base64.getDecoder().decode(""), 0, Role.USER)));
         assertEquals(2, jdbcUserDao.countAll());
     }
 
+    // FIXME
     @Test
+    @DisplayName("Adds user with the null email")
     void addWithNullEmailTest() {
         assertFalse(jdbcUserDao.add(new User(null,
                 "HASH2", Base64.getDecoder().decode(""), 0, Role.USER)));
