f0515db785a150dad7f7b137fcfbdf8c8812e407
edt77 <edt77@ukr.net>
2021-05-14 13:32:36 +0300

    Get All Stores:   - script to add shops.link_to_small_image column;   - insert into shops commands are moved to new V1_2* script;

---
+++ src/main/resources/db/migration/V1_2_add_Shops.sql
@@ -0,0 +1,5 @@
+insert into shops (link_to_main_page, link_to_image, name, link_to_small_image)
+values ('http://vinyl.ua/', 'img/shops/Vinyl_ua_logo.png', 'VinylUa', 'img/shops_small/Vinyl.ua.png');
+
+insert into shops (link_to_main_page, link_to_image, name, link_to_small_image)
+values ('https://www.juno.co.uk/', 'img/shops/junorecords_logo.png', 'JunoCoUk', 'img/shops_small/junorecords.png');
