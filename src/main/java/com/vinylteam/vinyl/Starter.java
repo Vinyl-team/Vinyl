36c495072f96c730d5e42af79d1893faa5be309d
edt77 <edt77@ukr.net>
2021-05-21 15:45:14 +0300

    clone.nl parser, implementation:  - CloneNlParser class added with logic for gathering data from clone.nl;  - CloneNlParser is added into parsers list in Starter;  - Test and ITest classes added for CloneNlParser;  - junit-jupiter-params is added as test dependency;  - PriceUtils class is added to process price string received from the site, the class is covered by tests;

--- src/main/java/com/vinylteam/vinyl/Starter.java
+++ src/main/java/com/vinylteam/vinyl/Starter.java
@@ -8,6 +8,7 @@ import com.vinylteam.vinyl.security.impl.DefaultSecurityService;
 import com.vinylteam.vinyl.service.*;
 import com.vinylteam.vinyl.service.impl.*;
 import com.vinylteam.vinyl.util.*;
+import com.vinylteam.vinyl.util.impl.CloneNlParser;
 import com.vinylteam.vinyl.util.impl.JunoVinylParser;
 import com.vinylteam.vinyl.util.impl.VinylUaParser;
 import com.vinylteam.vinyl.web.filter.SecurityFilter;
@@ -77,7 +78,7 @@ public class Starter {
 //UTIL, FILL IN DATABASE
         ShopsParser shopsParser = new ShopsParser();
         RawOffersSorter rawOffersSorter = new RawOffersSorter();
-        List<VinylParser> vinylParsers = List.of(new VinylUaParser(), new JunoVinylParser());
+        List<VinylParser> vinylParsers = List.of(new VinylUaParser(), new JunoVinylParser(), new CloneNlParser());
         Updater updater = new Updater(uniqueVinylService, offerService, shopsParser, vinylParsers, rawOffersSorter);
         TimerTask updateTask = new TimerTask() {
             @Override
